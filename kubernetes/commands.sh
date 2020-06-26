db40bb295dde4d77cc35d12105619053ef0124f5
cinexin <migueguillamon@gmail.com>
2020-06-26 21:06:24 +0200

    kubernetes basic commands

---
+++ kubernetes/commands.sh
@@ -0,0 +1,10 @@
+# Run an image
+kubectl run my-nginx --image nginx
+# Get running pods
+kubectl get pods
+
+#Get all objects
+kubectl get all
+
+# Remove a deployment
+kubectl delete deployment my-nginx
